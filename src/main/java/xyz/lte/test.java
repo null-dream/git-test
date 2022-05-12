661471f9f9c61d31ff1efa4312a31493b20cdd54
lte <2055103921@qq.com>
2022-05-12 15:26:39 +0800

    first commit

--- src/main/java/xyz/lte/test.java
+++ src/main/java/xyz/lte/test.java
@@ -3,6 +3,10 @@ package xyz.lte;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+
+        System.out.println("-----Hello Git------");
+        System.out.println("-----Hello Git------");
+        System.out.println("-----Hello Git------");
+
     }
 }
