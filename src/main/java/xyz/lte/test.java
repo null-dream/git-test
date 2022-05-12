a8b959ac589ef27bfc7b81421c4eb2e809e37389
lte <2055103921@qq.com>
2022-05-12 15:29:02 +0800

    first commit

--- src/main/java/xyz/lte/test.java
+++ src/main/java/xyz/lte/test.java
@@ -7,6 +7,7 @@ public class test {
         System.out.println("-----Hello Git------");
         System.out.println("-----Hello Git------");
         System.out.println("-----Hello Git------");
+        System.out.println("..............");
 
     }
 }
